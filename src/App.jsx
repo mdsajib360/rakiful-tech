426908a940d8325ed9fb23421ffe09b9eb8e0eef
Sajib Boss <sajib360.dev@gmail.com>
2025-11-30 23:39:45 +0600

    navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './Components/Nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<button class="btn btn-primary">Button</button>
-
+      <Nav/>
     </>
   )
 }
